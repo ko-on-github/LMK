d57157d958ed7314f0b5462706a72431ad90423f
ko-on-github <email.kennethong@gmail.com>
2020-01-11 11:34:34 +0800

    experiment

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+
 import matplotlib
 from lmk.ticker import Ticker
 
 matplotlib.rcParams['figure.figsize'] = (19, 8)
 
-ticker = Ticker("AAPL")
-ticker.retrieve_history("2000-06-01", "2019-12-15", "M") # Start date "yyyy-mm-dd", end date, frequency ("D", "W", "M")
+ticker = Ticker("^GSPC")
+ticker.retrieve_history("1990-01-01", "2020-1-5", "M") # Start date "yyyy-mm-dd", end date, frequency ("D", "W", "M")
 ticker.visualize("V,C,CL,LMK,WM,PV")
 
 # To run, in terminal python main.py when you are in LMK folder
+# To run spyder, in terminal spyder main.py when you are in LMK folder
+
+# Experiment
